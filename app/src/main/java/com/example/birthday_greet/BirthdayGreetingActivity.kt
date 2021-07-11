a1d47b031aaab65a9615873f3cb5ae34a643a683
Archit Jain <f20201994@pilani.bits-pilani.ac.in>
2021-07-11 13:32:17 +0530

    Initial commit

---
+++ app/src/main/java/com/example/birthday_greet/BirthdayGreetingActivity.kt
@@ -0,0 +1,12 @@
+package com.example.birthday_greet
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BirthdayGreetingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_birthday_greeting)
+
+    }
+}
